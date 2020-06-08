5d9fcc547a16d4397b18e0d22a1fe89e041e5921
Pandacore <44470187+Pandacore@users.noreply.github.com>
2020-06-08 17:09:09 +0100

    Test 2 green code

--- src/main/java/Template.java
+++ src/main/java/Template.java
@@ -4,6 +4,9 @@ import java.util.Map;
 public class Template {
 
     public String parse(String template, Map<String, String> variables) {
+        if(!variables.isEmpty()){
+            return template.replace("`$user`","Carlos");
+        }
         return template;
     }
 }
